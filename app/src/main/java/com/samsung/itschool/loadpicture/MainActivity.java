3b49833d88405f43fc09050673b548ff92c3d0f4
Ivan <ivan.kiel@gmail.com>
2020-12-25 18:38:19 +0800

    Завершение прототипа с загрузкой картинок

--- app/src/main/java/com/samsung/itschool/loadpicture/MainActivity.java
+++ app/src/main/java/com/samsung/itschool/loadpicture/MainActivity.java
@@ -32,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
             "https://img1.fonwall.ru/o/bq/space-atmosphere-space-kcep.jpg"
     };
 
-    ArrayList<ImageView> imageView = new ArrayList();
+    ArrayList<ImageView> imageViews = new ArrayList();
+    AsyncImageLoader loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,16 +49,14 @@ public class MainActivity extends AppCompatActivity {
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-/*                for (int i = 0; i < imageUrls.length; i++) {
-                    try {
-                        Bitmap image = getImageByURL(imageUrls[i]);
-                    } catch (MalformedURLException e){
-                        Toast.makeText(getApplicationContext(), "Неверный адрес", Toast.LENGTH_SHORT).show();
-                    }catch (IOException e) {
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }*/
-
+                if(loader == null || loader.getStatus() == AsyncTask.Status.FINISHED){
+                    loader = new AsyncImageLoader();
+                    loader.execute(imageUrls);
+                    progress.setProgress(0);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Подождите окончания загрузки",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -65,37 +65,66 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpView(){
         for (int i = 0; i < imageRow.getChildCount(); i++) {
-            imageView.add((ImageView) imageRow.getChildAt(i));
+            imageViews.add((ImageView) imageRow.getChildAt(i));
         }
     }
 
     class AsyncImageLoader extends AsyncTask<String, Pair<Integer, Bitmap>, Void>{
-
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
+        boolean isInternet = true;
+        String messageError = "";
         @Override
         protected Void doInBackground(String... strings) {
-            //TODO вся работа параллельного потока
+            for (int i = 0; i < strings.length; i++) {
+                try {
+                    Bitmap image = getImageByURL(strings[i]);
+                    Pair<Integer, Bitmap> pair = new Pair<>(i, image);
+                    publishProgress(pair);
+                } catch (MalformedURLException e){
+                    //Toast.makeText(getApplicationContext(),"Неверный адрес", Toast.LENGTH_SHORT).show();
+                    messageError = "Неверный адрес";
+                    isInternet = false;
+                    Pair<Integer, Bitmap> pair = new Pair<>(0, null);
+                    publishProgress(pair);
+                }catch (IOException e) {
+                    //Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
+                    messageError = e.getMessage();
+                    isInternet = false;
+                    Pair<Integer, Bitmap> pair = new Pair<>(0, null);
+                    publishProgress(pair);
+                }
+            }
             return null;
+        }
+
+        private Bitmap getImageByURL(String url) throws MalformedURLException, IOException{
+            Bitmap image = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            return image;
         }
 
         @Override
         protected void onProgressUpdate(Pair<Integer, Bitmap>... values) {
             super.onProgressUpdate(values);
+            if(isInternet) {
+                int position = values[0].first;//порядковый номер загруженной картинки
+                int currentProgress = position + 1;//для ProgressBar
+                Bitmap image = values[0].second;
+                //обновляем прогресс-бар
+                progress.setProgress(currentProgress * 100);
+                //показ картинки вариант 1
+                imageViews.get(position).setImageBitmap(image);
+                //показ картинки вариант 2 (без предварительного массива)
+                ((ImageView) imageRow.getChildAt(position)).setImageBitmap(image);
+            }else {
+                Toast.makeText(getApplicationContext(), messageError, Toast.LENGTH_SHORT).show();
+            }
+
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-        }
-
-        private Bitmap getImageByURL(String url)throws MalformedURLException, IOException{
-            Bitmap image = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
-            return image;
+            Toast.makeText(getApplicationContext(), "Загрука завершена", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
